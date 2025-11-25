c54137acde3b1340e9ada1c8bb867e969f96a8d3
KikyAja31 <2411500032@mahasiswa.atmaluhur.ac.id>
2025-11-25 12:25:16 +0700

    edit: index.php

--- index.php
+++ index.php
@@ -97,7 +97,8 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'system';
+	$system_path = 'vendor/pocketarc/codeigniter/system';
+ /*ganti ke system untuk yang biasa */
 
 /*
  *---------------------------------------------------------------
